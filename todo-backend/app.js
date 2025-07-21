97191e38d9027dc77ecda550bf993cc900a6a99e
Vansh nigam <vanshnigam8081432513@gmail.com>
2025-07-21 13:22:50 +0530

    commit

---
+++ todo-backend/app.js
@@ -0,0 +1,35 @@
+// Core Module
+const path = require('path');
+
+// External Module
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+const DB_PATH = "mongodb+srv://love:love@todo.juh47uw.mongodb.net/?retryWrites=true&w=majority&appName=Todo";
+
+const app = express();
+const errorsController=require('./controllers/errors')
+
+const  todoItemsRouter= require("./routes/todoItemsRouter");
+const cors = require('cors');
+
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(cors())
+
+app.use("/api/todo",todoItemsRouter)
+app.use(errorsController.pageNotFound);
+
+const PORT = 3001;
+
+mongoose.connect(DB_PATH)
+  .then(() => {
+    console.log('✅ Connected to Mongo');
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('❌ MongoDB connection failed:', err.message);
+  });
+
